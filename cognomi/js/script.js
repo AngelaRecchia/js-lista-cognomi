799331f3d93ea4aee9cd8c44862c0b1843624484
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-05-31 15:45:06 +0200

    add upper & lower case

--- cognomi/js/script.js
+++ cognomi/js/script.js
@@ -1,13 +1,15 @@
 var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 
 var cognUtente = prompt("Inserisci il tuo cognome.");
-cognUtente = cognUtente.charAt(0).toUpperCase() + cognUtente.slice(1);
+
+cognUtente = cognUtente.charAt(0).toUpperCase() + cognUtente.slice(1).toLowerCase();
+
 cognomi.push(cognUtente);
 cognomi = cognomi.sort();
 
 var i = 0;
 while (cognUtente != cognomi[i]) {
-    i++;
+     i++;
 }
 
 document.getElementById("text").innerHTML = cognomi + "</br> Il tuo cognome è il numero " + (i+1) + " nella lista.";
